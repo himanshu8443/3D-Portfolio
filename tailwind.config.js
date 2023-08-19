ba9e1c7ce381e1aabfdade0cf5044d51899c30d0
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-08-19 11:55:35 +0530

    Required assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "hero-pattern": "url('/src/assets/herobg.png')",
       },
     },
   },
