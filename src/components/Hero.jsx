e88b689cfba0252a38b84f85156cc6dde6b521bc
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-08-20 16:55:27 +0530

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+'use client'
+import { motion } from "framer-motion";
+
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto sm:px-16 px-6 flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
 
-export default Hero
+        <div>
+          <h1 className={`font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Himanshu</span>
+          </h1>
+          <p className={` text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2`}>
+            I develop 3D visuals, user <br className='sm:block hidden' />
+            interfaces and web applications
+          </p>
+        </div>
+      </div>
+      
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center z-30 cursor-pointer'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 cursor-pointer'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/hoc/SectionWrapper.jsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+
+import { staggerContainer } from "../../utils/motion";
+
+const StarWrapper = (Component, idName) =>
+  function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: true, amount: 0.25 }}
+        className={`sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0`}
+      >
+        <span className='hash-span' id={idName}>
+          &nbsp;
+        </span>
+
+        <Component />
+      </motion.section>
+    );
+  };
+
+export default StarWrapper;
