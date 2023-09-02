d5e6c819da7eb21ad97d8f36a725c838c7e64ea3
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-09-02 18:59:23 +0530

    GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,33 @@
-import { Toaster } from 'react-hot-toast'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Himanshu Sangwan',
-  description: 'Himanshu Sangwan is a full stack developer.', 
-}
+  title: "Himanshu Sangwan",
+  description: "Himanshu Sangwan is a full stack developer.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NRS9PX9R2K"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-NRS9PX9R2K');
+        `}
+      </Script>
       <body className={inter.className}>
-        <Toaster/>
+        <Toaster />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
